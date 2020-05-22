986d9ad436f83ce9706d85b3f117d8537cedb9d1
shijx <823961427@qq.com>
2020-05-22 11:05:37 +0800

    组合总数2

--- go/0040.combination-sum-ii/combination-sum-ii.go
+++ go/0040.combination-sum-ii/combination-sum-ii.go
@@ -1,43 +1,75 @@
-package problem0040
+package main
 
-import "sort"
+import (
+	"sort"
+)
+
+//给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+//
+// candidates 中的每个数字在每个组合中只能使用一次。
+//
+// 说明：
+//
+//
+// 所有数字（包括目标数）都是正整数。
+// 解集不能包含重复的组合。
+//
+//
+// 示例 1:
+//
+// 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+//所求解集为:
+//[
+//  [1, 7],
+//  [1, 2, 5],
+//  [2, 6],
+//  [1, 1, 6]
+//]
+//
+//
+// 示例 2:
+//
+// 输入: candidates = [2,5,2,1,2], target = 5,
+//所求解集为:
+//[
+//  [1,2,2],
+//  [5]
+//]
+// Related Topics 数组 回溯算法
 
 func combinationSum2(candidates []int, target int) [][]int {
 	sort.Ints(candidates)
+	res := make([][]int, 0)
+	tmp := make([]int, 0)
 
-	res := [][]int{}
-	solution := []int{}
-	cs2(candidates, solution, target, &res)
-
+	recursive(candidates, target, 0, tmp, &res)
 	return res
 }
 
-func cs2(candidates, solution []int, target int, result *[][]int) {
+func recursive(candidates []int, target int, cur int, tmp []int, res *[][]int) {
 	if target == 0 {
-		*result = append(*result, solution)
-	}
-
-	if len(candidates) == 0 || target < candidates[0] {
-		// target < candidates[0] 因为candidates是排序好的
+		*res = append(*res, tmp)
 		return
 	}
 
-	// 这样处理一下的用意是，让切片的容量等于长度，以后append的时候，会分配新的底层数组
-	// 避免多处同时对底层数组进行修改，产生错误的答案。
-	// 可以注释掉以下语句，运行单元测试，查看错误发生。
-	solution = solution[:len(solution):len(solution)]
+	n := len(candidates)
 
-	// 去掉已使用了的candidates[0]
-	cs2(candidates[1:], append(solution, candidates[0]), target-candidates[0], result)
+	for i := cur; i < n; i++ {
+		row := i
+		// 跳过当前循环的重复项
+		for i < n-1 && candidates[i] == candidates[i+1] {
+			i++
+		}
 
-	// 不使用candidates[0]的话，就要把所有和candidates[0]相等的元素都去掉。
-	cs2(next(candidates), solution, target, result)
-}
+		// 当前项大于目标值返回
+		if candidates[i] > target {
+			return
+		}
 
-func next(candidates []int) []int {
-	i := 0
-	for i+1 < len(candidates) && candidates[i] == candidates[i+1] {
-		i++
+		new1 := make([]int, len(tmp))
+		copy(new1, tmp)
+		new1 = append(new1, candidates[i])
+		recursive(candidates, target-candidates[i], row+1, new1, res)
+
 	}
-	return candidates[i+1:]
 }
