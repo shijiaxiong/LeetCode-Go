6e4411f540dc91449ecd160980ede12d1ba34417
shijiaxiong <shijiaxiong@soyoung.com>
2020-12-16 22:49:11 +0800

    test

--- go/0122.best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
+++ go/0122.best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -1,5 +1,18 @@
 package problem0122
 
+//贪心算法
 func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
 
+	res := 0
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-prices[i-1] > 0 {
+			res += prices[i] - prices[i-1]
+		}
+	}
+
+	return res
 }
