03325d69594861f64522c9c8d6f60447ea6d1a42
shijiaxiong <shijiaxiong@soyoung.com>
2020-12-25 15:42:57 +0800

    0387 第一个不唯一的字符串

---
+++ go/0387.first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -0,0 +1,16 @@
+package problem0387
+
+func firstUniqChar(s string) int {
+	result := make([]int, 26)
+	for i := 0; i < len(s); i++ {
+		result[s[i]-'a']++
+	}
+
+	for i := 0; i < len(s); i++ {
+		if result[s[i]-'a'] == 1 {
+			return i
+		}
+	}
+
+	return -1
+}
