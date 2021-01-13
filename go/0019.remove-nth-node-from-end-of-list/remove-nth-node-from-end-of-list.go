307cc65af5c80735a74c2bea8aee3dbfcc9b196d
shijiaxiong <shijiaxiong@soyoung.com>
2021-01-13 18:03:57 +0800

    旋转链表

--- go/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ go/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -1,6 +1,5 @@
 package main
 
-
 //type ListNode utils.ListNode
 type ListNode struct {
 	Val  int
@@ -8,14 +7,15 @@ type ListNode struct {
 }
 
 // 双指针
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
+func removeNthFromEnd0(head *ListNode, n int) *ListNode {
+	// 设置一个0的空节点 避免了待删除节点是head的情况
 	pre := &ListNode{}
 	pre.Next = head
 
-	start,end := pre,pre
+	start, end := pre, pre
 
-	// 先移动一个指针使得start end之间相隔n
-	for i:=0;i<n;i++{
+	// 先移动一个指针使得start end之间相隔n-1
+	for i := 0; i < n; i++ {
 		start = start.Next
 	}
 
@@ -29,4 +29,26 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	end.Next = end.Next.Next
 
 	return pre.Next
+}
+
+// 不设置空节点的双指针写法
+func removeNthFromEnd1(head *ListNode, n int) *ListNode {
+	start, end := head, head
+	for i := 0; i < n; i++ {
+		start  = start.Next
+	}
+
+	// 需要判断先走的指针是否走过了链表的尾部，这个时候删除的元素的head,所以返回head.next
+	if start == nil {
+		return head.Next
+	}
+
+	for start.Next != nil {
+		start = start.Next
+		end = end.Next
+	}
+
+	end.Next = end.Next.Next
+
+	return head
 }
---
+++ go/0061.rotate-list/rotate-list.go
@@ -0,0 +1,48 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 类似第19题
+// 双指针
+func rotateRight(head *ListNode, k int) *ListNode {
+	// head为nil、head只有一个元素、k为0直接返回
+	if head == nil || k == 0 || head.Next == nil {
+		return head
+	}
+
+	// 计算链表的长度
+	temp := head
+	count := 0
+	for temp != nil {
+		temp = temp.Next
+		count++
+	}
+
+	// k 大于链表长度取模
+	k = k % count
+	if k == 0 {
+		return head
+	}
+
+	// 双指针
+	start, end := head, head
+
+	for i := 0; i < k; i++ {
+		start = start.Next
+	}
+
+	for start.Next != nil {
+		start = start.Next
+		end = end.Next
+	}
+	
+	res := end.Next
+	// 不置为空就会形成环
+	end.Next = nil
+	start.Next = head
+
+	return res
+}
