5970907c79409017bcb6061569ca1b206f888944
shijiaxiong <shijiaxiong@soyoung.com>
2021-01-14 21:47:23 +0800

    环形链表

---
+++ go/142.linked-list-cycle-ii/linked-list-cycle-ii.go
@@ -0,0 +1,32 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+
+	for {
+		if fast == nil || fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			break
+		}
+	}
+
+	fast = head
+
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return fast
+}
