b20cec9170204f5144643a8081af42e976b9f1c8
shijiaxiong <shijiaxiong@soyoung.com>
2021-01-21 23:08:32 +0800

    练习

---
+++ go/0024.swap-nodes-in-pairs/swap-nodes-in-pairs.go
@@ -0,0 +1,26 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/dong-hua-yan-shi-24-liang-liang-jiao-huan-lian-bia/
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	//假设链表是 1->2->3->4
+	//这句就先保存节点2
+	next := head.Next
+
+	//继续递归，处理节点3->4
+	//当递归结束返回后，就变成了4->3
+	//于是head节点就指向了4，变成1->4->3
+	head.Next = swapPairs(next.Next)
+
+	//将2节点指向1
+	next.Next = head
+
+	return next
+}
