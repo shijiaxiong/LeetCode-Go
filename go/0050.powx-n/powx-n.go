8e998b31eeb378862580fc5fa4c509571feaa23a
shijiaxiong <shijiaxiong@soyoung.com>
2021-03-15 23:20:52 +0800

    test

--- go/0050.powx-n/powx-n.go
+++ go/0050.powx-n/powx-n.go
@@ -30,15 +30,15 @@ func quickMul(x float64, n int) float64 {
 func quickMul0(x float64, N int) float64 {
 	ans := 1.0
 	// 贡献的初始值为 x
-	x_contribute := x
+	pow := x
 	// 在对 N 进行二进制拆分的同时计算答案
 	for N > 0 {
 		if N % 2 == 1 {
 			// 如果 N 二进制表示的最低位为 1，那么需要计入贡献
-			ans *= x_contribute
+			ans *= pow
 		}
 		// 将贡献不断地平方
-		x_contribute *= x_contribute
+		pow *= pow
 		// 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
 		N /= 2
 	}
