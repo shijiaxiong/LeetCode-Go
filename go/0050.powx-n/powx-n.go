d2695aed612cc385d2953c60fe4b1a5bad2483c5
shijiaxiong <shijiaxiong@soyoung.com>
2021-03-16 22:00:24 +0800

    pow

--- go/0050.powx-n/powx-n.go
+++ go/0050.powx-n/powx-n.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // 递归
 // 时间复杂度：O(logn)
 // 空间复杂度：O(logn)
@@ -29,18 +33,44 @@ func quickMul(x float64, n int) float64 {
 // 空间复杂度：1
 func quickMul0(x float64, N int) float64 {
 	ans := 1.0
-	// 贡献的初始值为 x
-	pow := x
 	// 在对 N 进行二进制拆分的同时计算答案
 	for N > 0 {
-		if N % 2 == 1 {
+		if N%2 == 1 {
 			// 如果 N 二进制表示的最低位为 1，那么需要计入贡献
-			ans *= pow
+			ans *= x
 		}
+
 		// 将贡献不断地平方
-		pow *= pow
+		x *= x
 		// 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
 		N /= 2
 	}
 	return ans
+}
+
+// 直接迭代
+func myPow0(x float64, n int) float64 {
+	if n < 0 {
+		x = 1.0 / x
+		n = -n
+	}
+
+	ans := 1.0
+
+	for n > 0 {
+		if n%2 == 1 {
+
+			ans *= x
+		}
+		fmt.Println(n)
+
+		x *= x
+		n >>= 1
+	}
+
+	return ans
+}
+
+func main() {
+	myPow0(10.0, 4)
 }
