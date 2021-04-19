b840c6b2cc8c30d44552d896a061661a6f1fe32b
shijiaxiong <shijiaxiong@soyoung.com>
2021-04-19 11:10:25 +0800

    最长回文子串 反转链表

--- go/0005.longest-palindromic-substring/longest-palindromic-substring.go
+++ go/0005.longest-palindromic-substring/longest-palindromic-substring.go
@@ -1,39 +1,46 @@
 package main
 
+// https://leetcode-cn.com/problems/longest-palindromic-substring/solution/5-zui-chang-hui-wen-zi-chuan-dong-tai-gui-hua-jie-/
 func longestPalindrome(s string) string {
+	// 过滤单个字符的场景
 	length := len(s)
 
 	if length < 2 {
 		return s
 	}
 
-	maxLen, begin := 1, 0
+	result := [2]int{}
+
 	dp := make([][]bool, length)
 
 	for i := 0; i < length; i++ {
 		dp[i] = make([]bool, length)
 
+		// 在dp数组图中符合条件的只是对称线上的点
 		dp[i][i] = true
 	}
 
-	for j := 1; j < length; j++ {
-		for i := 0; i < j; i++ {
-			if s[i] != s[j] {
-				dp[i][j] = false
-			} else {
-				if j-i < 3 {
+	// 从右下角开始遍历，右下角往上一格开始
+	for i := length - 2; i >= 0; i-- {
+		for j := i + 1; j < length; j++ {
+			if s[i] == s[j] {
+				// i j相邻
+				if j-i == 1 {
 					dp[i][j] = true
 				} else {
 					dp[i][j] = dp[i+1][j-1]
 				}
+			} else {
+				dp[i][j] = false
 			}
+
 			//	只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
-			if dp[i][j] && j-i+1 > maxLen {
-				maxLen = j - i + 1
-				begin = i
+			if dp[i][j] && result[1]-result[0] <= j-i {
+				result[0] = i
+				result[1] = j
 			}
 		}
 	}
 
-	return s[begin : begin+maxLen]
+	return s[result[0] : result[1]+1]
 }
--- go/0206.reverse-linked-list/reverse-linked-list.go
+++ go/0206.reverse-linked-list/reverse-linked-list.go
@@ -4,14 +4,21 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
 // 递归
 func reverseList(head *ListNode) *ListNode {
+	// 递归终止条件是当前为空，或者下一个节点为空
 	if head == nil || head.Next == nil {
+		// 返回的head是cur的值
 		return head
 	}
 
+	// 这里的cur就是链表的最后一个节点
 	cur := reverseList(head.Next)
 
+	//如果链表是 1->2->3->4->5，那么此时的cur就是5
+	//而head是4，head的下一个是5，下下一个是空
+	//所以head.next.next 就是5->4
 	head.Next.Next = head
 
 	head.Next = nil
@@ -20,6 +27,7 @@ func reverseList(head *ListNode) *ListNode {
 }
 
 // 双指针
+// 使用了head代替cur，不熟悉不好理解 不推荐
 func reverseList0(head *ListNode) *ListNode {
 	var prev *ListNode
 
@@ -30,6 +38,22 @@ func reverseList0(head *ListNode) *ListNode {
 		prev = head
 
 		head = temp
+	}
+
+	return prev
+}
+
+// prev cur 同时移动
+// https://leetcode-cn.com/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-shuang-zhi-zhen-di-gui-yao-mo-/
+func reverseList1(head * ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+
+	for cur != nil {
+		temp := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = temp
 	}
 
 	return prev
